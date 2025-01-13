886c4ee56f0b0f510217a7d9e1d993faf798fb18
Lnola <nola.luka@gmail.com>
2025-01-13 15:58:16 +0000

    :tada: Add demo nodes from the book

---
+++ src/beginner_tutorials/scripts/pylistener.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import rospy
+from std_msgs import msg
+
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + " I heard %s", data.data)
+
+
+def listener():
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # name are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'talker' node so that multiple talkers can
+    # run simultaneously.
+    rospy.init_node("listener", anonymous=True)
+    rospy.Subscriber("chatter", msg.String, callback)
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+
+if __name__ == "__main__":
+    listener()
