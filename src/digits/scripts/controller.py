5b9cf83aa30f11a7ad8c054bf72a99c356c1476e
Lnola <nola.luka@gmail.com>
2025-01-14 02:42:01 +0000

    :sparkles: Add the segments publisher to the controller

--- src/digits/scripts/controller.py
+++ src/digits/scripts/controller.py
@@ -2,9 +2,24 @@
 
 import rospy
 import sys
+from digits.msg import Segments
 from std_srvs.srv import Empty
 
 SERVICE_CUSTOM_RESET = "/drawer/reset"
+TOPIC_SEGMENTS = "segments"
+
+segments_array = {
+    0: [0, 0, 0, 0, 0, 0, 1],
+    1: [1, 1, 0, 0, 1, 1, 1],
+    2: [1, 0, 0, 1, 0, 0, 0],
+    3: [1, 0, 0, 0, 0, 1, 0],
+    4: [0, 1, 0, 0, 1, 1, 0],
+    5: [0, 0, 1, 0, 0, 1, 0],
+    6: [0, 0, 1, 0, 0, 0, 0],
+    7: [1, 0, 0, 0, 1, 1, 1],
+    8: [0, 0, 0, 0, 0, 0, 0],
+    9: [0, 0, 0, 0, 0, 1, 0],
+}
 
 
 def draw_digit(digit):
@@ -16,6 +31,15 @@ def draw_digit(digit):
         reset_service = rospy.ServiceProxy(SERVICE_CUSTOM_RESET, Empty)
         reset_service()
         rospy.loginfo("Init custom reset of turtlesim.")
+
+        publisher = rospy.Publisher(TOPIC_SEGMENTS, Segments, queue_size=10)
+        msg = Segments()
+        msg.digit = digit
+        msg.segments = segments_array[digit]
+        rospy.sleep(0.1)
+        publisher.publish(msg)
+        rospy.loginfo(f"Send digit {digit} and the corresponding segments.")
+
     except rospy.ServiceException as e:
         rospy.logerr(f"Service call failed: {e}")
 
