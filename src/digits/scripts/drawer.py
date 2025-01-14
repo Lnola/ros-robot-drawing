2c676ee5b789d53685b467b010924b663a5b3422
Lnola <nola.luka@gmail.com>
2025-01-14 00:50:10 +0000

    :rotating_light: Fix the function definition and add logging

--- src/digits/scripts/drawer.py
+++ src/digits/scripts/drawer.py
@@ -3,9 +3,12 @@
 import rospy
 from std_srvs.srv import Empty
 
-def reset():
+def reset(_):
     reset_service = rospy.ServiceProxy("/reset", Empty)
     reset_service()
+    rospy.loginfo("Custom resetting turtlesim.")
+    return {}
+
 
 def draw():
     rospy.init_node('drawer', anonymous=True)
